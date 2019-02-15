8eac33bbd9b29cdfef4a41d4ef57799d03e536c6
gaoyj <gaoyj@xiaoshouyi.com>
2019-02-15 17:15:37 +0800

    增加数据库连接池和mybatis

---
+++ emerce-manage-web/src/main/java/com/yj/configuration/DataSourceConfiguation.java
@@ -0,0 +1,32 @@
+package com.yj.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@PropertySource("classpath:databasesource.properties")
+public class DataSourceConfiguation {
+
+    private Logger logger= LoggerFactory.getLogger(DataSourceConfiguation.class);
+
+   @Bean(name="dataSource")
+   @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource createDataSource(){
+        logger.info("create data source");
+        return DataSourceBuilder.create().build();
+    }
+
+
+}
+
+
+
+
