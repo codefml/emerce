c99632d6759a432809d66254f21eeba306803272
gaoyj <gaoyj@xiaoshouyi.com>
2019-03-01 16:48:33 +0800

    修正包冲突

--- emerce-manage-web/src/main/java/com/yj/AppStart.java
+++ emerce-manage-web/src/main/java/com/yj/AppStart.java
@@ -1,10 +1,12 @@
 package com.yj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 //MACBOOKPRO-FAAE
 @SpringBootApplication(scanBasePackages = {"com.yj"})
+@ServletComponentScan
 public class AppStart extends SpringBootServletInitializer{
     public static void main(String[] args) {
         SpringApplication.run(AppStart.class,args);
