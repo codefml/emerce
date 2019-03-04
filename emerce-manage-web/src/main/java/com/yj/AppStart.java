5212e4412ac09ed31599f100263d4cf7dc068567
gaoyj <24507675@qq.com>
2019-03-04 22:04:47 +0800

    调整启动方式

--- emerce-manage-web/src/main/java/com/yj/AppStart.java
+++ emerce-manage-web/src/main/java/com/yj/AppStart.java
@@ -1,6 +1,7 @@
 package com.yj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +9,15 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication(scanBasePackages = {"com.yj"})
 @ServletComponentScan
 public class AppStart extends SpringBootServletInitializer{
+
     public static void main(String[] args) {
         SpringApplication.run(AppStart.class,args);
     }
+
+
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // 注意这里要指向原先用main方法执行的Application启动类
+//        return builder.sources(AppStart.class);
+//    }
 }
