e8e23b4a8b572a32ede353a8ba8c88c665ac5f11
gaoyj <gaoyj@xiaoshouyi.com>
2019-03-06 18:30:47 +0800

    修正启动方式

--- emerce-manage-web/src/main/java/com/yj/AppStart.java
+++ emerce-manage-web/src/main/java/com/yj/AppStart.java
@@ -5,19 +5,24 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-//MACBOOKPRO-FAAE
+
+/**
+ * 方式一：默认的application启动，在创建项目时自动生成application启动类，直接run执行即可
+ * 方式二：默认的启动类要继承SpringBootServletInitiailzer类，并复写configure()方法
+ */
 @SpringBootApplication(scanBasePackages = {"com.yj"})
 @ServletComponentScan
 public class AppStart extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(AppStart.class,args);
+        //new SpringApplicationBuilder(AppStart.class).web(true).run(args);
     }
 
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        // 注意这里要指向原先用main方法执行的Application启动类
-//        return builder.sources(AppStart.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(AppStart.class);
+    }
 }
