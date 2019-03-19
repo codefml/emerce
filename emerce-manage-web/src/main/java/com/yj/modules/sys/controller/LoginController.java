4d922932270dfaefe0a875a2ecfb024f96942525
gaoyj <gaoyj@xiaoshouyi.com>
2019-03-19 18:37:23 +0800

    增加页面

--- emerce-manage-web/src/main/java/com/yj/modules/sys/controller/LoginController.java
+++ emerce-manage-web/src/main/java/com/yj/modules/sys/controller/LoginController.java
@@ -3,13 +3,14 @@ package com.yj.modules.sys.controller;
 import com.yj.modules.sys.entity.SysUser;
 import com.yj.modules.sys.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@Controller(value = "sysUser")
-@RestController
+@Controller(value = "sysUser")
+//@RestController
 //@RequestMapping(value="${adminPath}")
 public class LoginController {
     @Autowired
@@ -18,7 +19,7 @@ public class LoginController {
 
 
 
-    @RequestMapping(value="/login")
+    @RequestMapping(value="/logintest")
     public String login(){
         List<SysUser> list=sysUserService.queryList();
         for(SysUser sysUser:list){
